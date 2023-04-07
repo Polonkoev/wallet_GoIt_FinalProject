8fc2321d5250348a655b3f07288609f8b1e138ca
MariaFandeeva <mfandeeva@gmail.com>
2023-04-07 22:45:13 +0300

    swagger 1./

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const logger = require("morgan");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger.json");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const userRouter  = require("./routes/api/userRouter");
+const userRouter = require("./routes/api/userRouter");
 // const { transactionRouter } = require("./routes/api/transactionsRouter");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/users", userRouter);
 // app.use("/api/transactions", transactionsRouter);
 
