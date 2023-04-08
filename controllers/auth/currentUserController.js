ffd3ad7c3cb5aff2fa8f98b7359d8fd2635916e3
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2023-04-08 17:29:47 +0600

    bugfix/current user

--- controllers/auth/currentUserController.js
+++ controllers/auth/currentUserController.js
@@ -3,26 +3,26 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../../models/User");
 const { SECRET } = process.env;
 
- const currentUserController = async (req, res) => {    
-    try {
-        const user = await User.findById(req.user.id);
-        if (!user) {
-          return res.json({ message: "There is no such user" });
-        }
-    
-        const token = jwt.sign(
-          {
-            id: user._id,
-          },
-          SECRET,
-          { expiresIn: "30d" }
-        );
-    
-        res.json({ user, token });
-      } catch (error) {
-        res.json({ message: "Not allowed" });
-      }
+const currentUserController = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    const { email, name } = user;
+    if (!user) {
+      return res.json({ message: "There is no such user" });
+    }
 
-}
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      SECRET,
+      { expiresIn: "30d" }
+    );
+
+    res.json({ email, name, token });
+  } catch (error) {
+    res.json({ message: "Not allowed" });
+  }
+};
 
 module.exports = currentUserController;
