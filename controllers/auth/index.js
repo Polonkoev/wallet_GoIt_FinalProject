25cd7242dd140b11f754f696837cb26845681166
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2023-04-08 17:19:12 +0600

    Feature/currentUserController
    Добавлен энд поинт current. Исправлен id на _id в регистрации

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,9 +1,11 @@
 const signupController = require("./signupController");
 const loginController = require("./loginController");
 const logoutController = require("./logoutController");
+const currentUserController = require("./currentUserController")
 
 module.exports = {
   signupController,
   loginController,
   logoutController,
+  currentUserController
 };
